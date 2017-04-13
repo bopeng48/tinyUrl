c373cb33978135cd07e3c88d89313cfa94dff0e4
Bo <bopeng48@gmail.com>
2017-04-13 23:01:44 +0000

    finished feature: log in, log out, register. authetification

--- express_server.js
+++ express_server.js
@@ -38,6 +38,24 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+  "ran1": {
+    id: "ran1",
+    email: "1@1.com",
+    password: "1"
+  }
+}
+
 
 
 
@@ -75,18 +93,13 @@ app.get("/urls", (req, res) => {
   //   console.log(k);
   // }
   let templateVars = {
-  username: req.cookies["username"],
+  username: req.cookies["user_id"],
   // ... any other vars
   };
   res.render("urls_index.ejs", {templateVar: urlDatabase},templateVars);
 });
 
-  // let templateVars = { urls: urlDatabase };
-// console.log(templateVars.urls['b2xVn2']);
-// for(var key in templateVars.urls)
-// {
-//   console.log(templateVars.urls[key]);
-// }
+
 
 
 function generateRandomString() {
@@ -100,10 +113,10 @@ function generateRandomString() {
 app.get("/urls/new", (req, res) => {
 
   let templateVars = {
-  username: req.cookies["username"],
+  username: req.cookies["user_id"],
   // ... any other vars
 };
-console.log(req.cookies["username"]);
+console.log(req.cookies["user_id"]);
   res.render("urls_new",templateVars);
 });
 
@@ -144,7 +157,7 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   // let templateVar = { shortURL: req.params.id};  not used
   let templateVars = {
-  username: req.cookies["username"],
+  username: req.cookies["user_id"],
   // ... any other vars
 };
 
@@ -167,33 +180,122 @@ app.post("/urls/:id", (req, res) => {
 
 app.get("/login", (req, res) => {
 
-  console.log(req.cookies);
+  console.log("*** in get /login ***");
+  console.log("req.cookies is ",req.cookies);
+
+  let user = users[req.cookies.user_id];
+
+
+
   let templateVars = {
-    username: req.cookies.username,
+    username: user
   // ... any other vars
   };
+
+
   console.log("currently "+templateVars.username+" is logged in");
   res.render("./partials/_header.ejs",templateVars);
 })
 
 
 
+
+
+// this function handles the authetification process
 app.post("/login", (req, res) => {
-  console.log(req.body.username);
-  res.cookie('username',req.body.username);
-  res.redirect("/");
+
+  console.log("in post /login")
+
+
+  let userName = req.body.username;
+  let password = req.body.password;
+
+  console.log("password entered is",password);
+
+  for(let k in users) {
+    if(users[k]["email"] === userName) {
+      console.log("username authetification success");
+      console.log(users[k]);
+      console.log(users[k]["password"], typeof users[k]["password"]);
+      if(users[k]["password"] === password) {
+        console.log("passWord authetification success");
+        res.cookie('user_id',k);
+        console.log("login sucess");
+        res.redirect("/");
+        } else {
+          res.status(404).send("password don't match");
+        }
+      }
+    }
+  res.status(404).send("entered incorrect credentials");
 })
 
+
+
+
+
+
+
+
+
+
+
 app.post("/logout/:username", (req, res) => {
-  let user = req.params.username;
+  let user = req.params.user_id;
   console.log(user, "is trying to log out");
   console.log(typeof user);
-  res.clearCookie("username",user);
+  res.clearCookie("user_id",user);
   console.log(user, "is looged out");
   res.redirect("/login");
 })
 
 
+// handling registration
+
+function createUser(inputID,inputEmail,inputPassword){
+  let user = {
+    id: inputID,
+    email: inputEmail,
+    password: inputPassword
+  };
+  return user;
+}
+
+app.get("/register", (req, res) => {
+  res.render("./urls_register.ejs");
+})
+
+app.post("/register", (req, res)=> {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  if(email ==="" || password ==="") {
+    res.status(404).send("fields can't be empty");
+  }
+
+  for(var key in users) {
+    console.log(typeof users[key], users[key]);
+    if(users[key].email === email) {
+      res.status(404).send("you've already registered");
+    }
+  }
+
+  console.log("there's no duplicate");
+
+
+
+
+
+
+  let id = generateRandomString();
+
+  users[id] = createUser(id,email,password);
+  res.cookie("user_id",id);
+  console.log(users[id]);
+  console.log(users);
+
+  res.redirect("/");
+})
 
 
 
